462925276422ef8789f7d699a1f49ac8a18cfe00
maksudbelim <dev@techsolutionslab.com>
2021-04-23 12:25:30 +0530

    pharma changes
    pharma changes

--- app/Http/Controllers/OutfordeliveryController.php
+++ app/Http/Controllers/OutfordeliveryController.php
@@ -54,7 +54,7 @@ class OutfordeliveryController extends Controller
 		$per_page=(isset($_POST['perpage']) && $_POST['perpage']!='')?$_POST['perpage']:10;
 		$searchtxt=(isset($_POST['searchtxt']) && $_POST['searchtxt']!='')?$_POST['searchtxt']:'';
 		
-		$order_detail = new_orders::select('new_orders.id','assign_datetime','order_number','deliveryboy_id','new_users.name as customer_name','new_users.mobile_number as customer_number','new_users.id as customerid', 'prescription.name as prescription_name', 'prescription.image as prescription_image', 'delivery_user.name as assign_to', 'logistic.name as logistic_name', 'logistic.code as logistic_code')
+		$order_detail = new_orders::select('new_orders.id','assign_datetime','order_number','deliveryboy_id','new_users.name as customer_name','new_users.mobile_number as customer_number','new_users.id as customerid', 'prescription.name as prescription_name', 'prescription.image as prescription_image', 'delivery_user.name as assign_to', 'logistic.name as logistic_name', 'logistic.code as logistic_code','new_orders.is_external_delivery as is_external_delivery','new_orders.logistic_user_id as logistic_user_id')
 		->leftJoin('new_users', 'new_users.id', '=', 'new_orders.customer_id')
 		->leftJoin('new_logistics AS logistic', 'logistic.id', '=', 'new_orders.logistic_user_id')
 		->leftJoin('new_pharma_logistic_employee AS delivery_user', 'delivery_user.id', '=', 'new_orders.deliveryboy_id')
@@ -94,8 +94,13 @@ class OutfordeliveryController extends Controller
 					}
 				}
 				$time = get_order_time($order->id, $order->deliveryboy_id);
-				//$assign_to = ($order->assign_to !== null)?($order->assign_to):($order->logistic_code);
-				$assign_to = $order->logistic_code;
+				if($order->is_external_delivery == 0){
+					$assign_to = $order->assign_to;	
+				}else{
+					$assign_to = get_name('new_logistics','code',$order->logistic_user_id);
+				}
+				
+				//$assign_to = $order->logistic_code;
 				$html.='<tr>
 					<td>'.ucwords(strtolower($order->customer_name)).'</td>
 					<td><a href="'.url('/orders/order_details/'.$order->id).'"><span>'.$order->order_number.'</span></a></td>
