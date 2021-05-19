043901f2643ee20d33c39cb7211a5168041786bb
poonamkeshariya <poonam.k@clientsdemosite.in>
2021-05-19 18:37:00 +0530

    commit
    commit

--- app/Http/Controllers/DeliveryboyController.php
+++ app/Http/Controllers/DeliveryboyController.php
@@ -339,6 +339,7 @@ class DeliveryboyController extends Controller
 		}
 		$user = new_pharma_logistic_employee::find($id);
 		$user->is_active=0;
+		$user->is_delete='1';
 		$user->mobile_number='';
 		$user->email='';
 		$user->save();
