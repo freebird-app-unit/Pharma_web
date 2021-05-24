478e20901d0c6b796680f65fd15b30f8145b5274
poonamkeshariya <poonam.k@clientsdemosite.in>
2021-05-24 13:26:33 +0530

    commit
    commit

---
+++ app/area_covered_single_logistics.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class area_covered_single_logistics extends Model
+{
+    protected $table = 'area_covered_single_logistics';
+}
