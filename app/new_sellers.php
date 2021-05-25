c7d59eb04052bee8dfabb6aa58f8ebb7ba3cd4e5
poonamkeshariya <poonam.k@clientsdemosite.in>
2021-05-25 07:05:01 +0530

    commit
    commit

--- app/new_sellers.php
+++ app/new_sellers.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
-class new_sellers extends Model
+class new_sellers extends Authenticatable
 {
+	use Notifiable, HasApiTokens;
      protected $table = 'new_sellers';
 }
